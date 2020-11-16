18d594f3bbf5989db705e5b9c391d49193314b29
Luis Belloch <luis.bellochmartinez@gmail.com>
2020-11-16 11:33:05 +0100

    Map working
    Still the location is not correctly shown

--- Android/app/src/main/java/com/example/recycle/activity/MainActivity.java
+++ Android/app/src/main/java/com/example/recycle/activity/MainActivity.java
@@ -9,12 +9,18 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
@@ -22,16 +28,31 @@ import com.example.recycle.R;
 import com.example.recycle.visual.Tab1;
 import com.example.recycle.visual.Tab2;
 import com.example.recycle.visual.Tab3;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        OnMapReadyCallback, GoogleMap.OnMapClickListener {
     // Nombres de las pestañas
-    private String[] nombres = new String[]{"Mapa","Inicio","Opciones"};
-    private int[] iconos = new int[]{R.drawable.ic_baseline_map_24 , R.drawable.ic_baseline_home_24 , R.drawable.ic_baseline_settings_24};
+    private String[] nombres = new String[]{"Mapa", "Inicio", "Opciones"};
+    private int[] iconos = new int[]{R.drawable.ic_baseline_map_24, R.drawable.ic_baseline_home_24, R.drawable.ic_baseline_settings_24};
 
-    // Location
-    private FusedLocationProviderClient fusedLocationClient;
+    // MAPA
+    private int locationRequestCode = 1000;
+    private double wayLatitude = 0.0, wayLongitude = 0.0;
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private Location location = null;
+    private LocationManager manejador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,17 +67,19 @@ public class MainActivity extends AppCompatActivity {
         new TabLayoutMediator(tabs, viewPager,
                 new TabLayoutMediator.TabConfigurationStrategy() {
                     @Override
-                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position){
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
                         tab.setText(nombres[position]);
                         tab.setIcon(iconos[position]);
                     }
                 }
         ).attach();
 
-        viewPager.setCurrentItem(1,false);
+        viewPager.setCurrentItem(1, false);
+
+
     }
 
-    public void onClickPerfil(View view){
+    public void onClickPerfil(View view) {
 
         Intent intent = new Intent(this, UsuarioActivity.class);
         startActivity(intent);
@@ -80,7 +103,73 @@ public class MainActivity extends AppCompatActivity {
         intent.setType("text/plain");
         intent.putExtra(Intent.EXTRA_SUBJECT, "asunto");
         intent.putExtra(Intent.EXTRA_TEXT, "texto del correo");
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"tostyfis360@gmail.com"});
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"tostyfis360@gmail.com"});
         startActivity(intent);
+    }
+
+    // MAPA
+    public void mapTabLoaded(SupportMapFragment supportMapFragment) {
+        // MAPA
+        manejador = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criterio = new Criteria();
+        criterio.setCostAllowed(false);
+        criterio.setAltitudeRequired(false);
+        criterio.setAccuracy(Criteria.ACCURACY_FINE);
+        String proveedor = manejador.getBestProvider(criterio, true);
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // request for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    locationRequestCode);
+
+        } else {
+            // already permission granted
+            mapFragment = (SupportMapFragment) supportMapFragment;
+            location = manejador.getLastKnownLocation(proveedor);
+            mapFragment.getMapAsync(this);
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1000: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    
+                } else {
+                    Toast.makeText(this, R.string.no_permission, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        map.getUiSettings().setZoomControlsEnabled(false);
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+
+        map.setOnMapClickListener(this);
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setCompassEnabled(true);
+        }
     }
 }
