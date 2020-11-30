a6f55e3b5187fc168e6e83fb7e031bd952277c1b
Luis Belloch <luis.bellochmartinez@gmail.com>
2020-11-30 15:11:14 +0100

    Gráficas de los cubos
    Falta la gráfica temporal

--- Android/app/src/main/java/com/example/recycle/activity/AdaptadorCubos.java
+++ Android/app/src/main/java/com/example/recycle/activity/AdaptadorCubos.java
@@ -1,6 +1,8 @@
 package com.example.recycle.activity;
 
 import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,10 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recycle.R;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +29,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AdaptadorCubos extends RecyclerView.Adapter<RecyclerViewHolder> {
 
@@ -81,8 +88,39 @@ public class AdaptadorCubos extends RecyclerView.Adapter<RecyclerViewHolder> {
                             // Si el usuario tiene cubos, tomamos nombres como referencia
                             if(nombres.size() > 0) {
                                 // Asignamos las variables a la vista del cubo
-                                if(holder.getHolderType().equals("cubo")) {
+                                if(holder.getHolderType() == 1) {
+                                    // Modificamos el cubo para mostrar los datos que queremos
+                                    // Nombre
                                     holder.getNombreCubo().setText(nombres.get(itemList));
+                                    // Gráfica plástico
+                                    List<BarEntry> plasticMeasures = new ArrayList<>();
+                                    // Position and value of the bar
+                                    plasticMeasures.add(new BarEntry(0f, Integer.parseInt(vidrio.get(itemList))));
+                                    plasticMeasures.add(new BarEntry(1f, Integer.parseInt(plastico.get(itemList))));
+                                    plasticMeasures.add(new BarEntry(2f, Integer.parseInt(organico.get(itemList))));
+                                    plasticMeasures.add(new BarEntry(3f, Integer.parseInt(carton.get(itemList))));
+                                    // Set the data
+                                    BarDataSet set = new BarDataSet(plasticMeasures, "Medidas");
+                                    // Propiedades de las barras
+                                    set.setValueTextSize(0f);
+                                    set.setHighlightEnabled(false);
+                                    // Ponemos los colores del set de datos
+                                    set.setColors(ColorTemplate.MATERIAL_COLORS);
+                                    // asignamos el set a los datos
+                                    BarData data = new BarData(set);
+                                    // Ajustes de estilo
+                                    holder.getPlasticChart().setDescription(null);
+                                    holder.getPlasticChart().setDrawGridBackground(false);
+                                    holder.getPlasticChart().getXAxis().setEnabled(false);
+                                    holder.getPlasticChart().getXAxis().setDrawLabels(false);
+                                    holder.getPlasticChart().getAxisLeft().setEnabled(false);
+                                    holder.getPlasticChart().getAxisLeft().setAxisMaximum(100);
+                                    holder.getPlasticChart().getAxisLeft().setAxisMinimum(0);
+                                    holder.getPlasticChart().getAxisRight().setEnabled(false);
+                                    holder.getPlasticChart().getLegend().setEnabled(false);
+                                    // Poner datos
+                                    holder.getPlasticChart().setData(data);
+                                    holder.getPlasticChart().invalidate(); // refresh
                                 }
                                 itemList++;
                                 // Se añade el boton de anyadir al final de la lista
--- Android/app/src/main/java/com/example/recycle/activity/RecyclerViewHolder.java
+++ Android/app/src/main/java/com/example/recycle/activity/RecyclerViewHolder.java
@@ -7,26 +7,40 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recycle.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
 
 public class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
     private TextView nombreCubo;
+    private BarChart plasticChart;
+    private LineChart lineChart;
+
     public RecyclerViewHolder(@NonNull View itemView) {
         super(itemView);
         nombreCubo = itemView.findViewById(R.id.nombreCubo);
+        plasticChart = itemView.findViewById(R.id.bar_chart);
+        lineChart = itemView.findViewById(R.id.line_chart);
     }
 
     // Devuelve si es un cubo o un boton en base a si tiene nombreCubo o no
-    public String getHolderType() {
+    public int getHolderType() {
         if(getNombreCubo() != null) {
-            return "cubo";
+            return 1; // Cubo
         } else {
-            return "boton";
+            return 2; // Boton
         }
     }
 
     // Devuelve la vista
     public TextView getNombreCubo(){
         return nombreCubo;
+    }
+
+    public BarChart getPlasticChart() {
+        return plasticChart;
+    }
+    public LineChart getLineChart() {
+        return lineChart;
     }
 }
