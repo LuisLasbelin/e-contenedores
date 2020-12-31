6334dab2b3fd57ba0d2645e954d1f49249546762
Jorge <jgragia@epsg.upv.es>
2020-12-31 01:14:57 +0100

    Calculadora de carbono e iconos de la aplicacion

---
+++ Android/app/src/main/java/com/example/recycle/activity/AyudaCalculadora.java
@@ -0,0 +1,18 @@
+package com.example.recycle.activity;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.recycle.R;
+
+public class AyudaCalculadora extends AppCompatActivity {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda);
+    }
+
+}
